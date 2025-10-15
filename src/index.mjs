b4e36f3feb36faf5395dc114c53f0cc4e3ad3724
Alex <stamelosalexios@gmail.com>
2025-10-15 09:16:55 +0000

    Sessions

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,23 @@
 import express, { response } from 'express';
 import routes from "./routes/index.mjs";
 import cookieParser from 'cookie-parser';
-
+import session from "express-session";
+import { mockusers } from './utils/constants.mjs';
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    session({
+        secret: 'password',
+        saveUninitialized: false,
+        resave: false,
+        cookie:{
+            maxAge: 60000 * 60,
+        } // 1hour 
+    })
+);
+// use middleware before routes
 app.use(routes);
 
 
@@ -13,12 +25,48 @@ const PORT = process.env.PORT || 3000; // if env port is undefined use 3000
 
 app.get("/", (request, response ) => {
     response.cookie("Maincookie", "world", {maxAge: 6000 * 60 * 2});
+    request.session.visited = true;
     response.status(200).send({msg: "json msg Hello"});
 
 });
 
 
+app.post('/api/auth', (request, response) => {
+    const { body: { username, password },
+ } = request;
+    const findUser = mockusers.find(
+        (user) => user.username === username
+    );
+    if (!findUser || findUser.password !== password) 
+        return response.status(401).send({ msg: "BAD CREDITIANLS"});
+    request.session.user = findUser;
+    return response.status(200).send(findUser);
 
+});
+
+app.get('/api/auth/status', (request, response) =>{
+    return request.session.user ? response.status(200).send(request.session.user) : response.status(401).send({ msg: "Not authenticated"});
+})
+
+// check if authorized if yes adds an item in the cart
+app.post("/api/cart", (request, response) => {
+    if (!request.session.user) return response.sendStatus(401);
+    const {body: item} = request;
+    const { cart } = request.session;
+
+    if (cart) {
+        cart.push(item);
+    }
+    else {
+        request.session.cart = [item];
+    }
+    return response.status(201).send(item);
+});
+
+app.get('/api/cart', (request, response) => {
+    if (!request.session.user) return response.sendStatus(401);
+    return response.send(request.session.cart ?? []);
+});
 
 app.listen(PORT, () => {
     console.log('Running on Port ' + PORT);
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,5 +1,5 @@
 export const mockusers = [
-        {id: 1, username: "anson", displayName:"Anson"},
-        {id: 2, username: "manoulakios", displayName:"Manos"},
-        {id: 3, username: "markos", displayName:"Mark"}
+        {id: 1, username: "anson", displayName:"Anson", password: "123"},
+        {id: 2, username: "manoulakios", displayName:"Manos", password: "322"},
+        {id: 3, username: "markos", displayName:"Mark", password: "pasok"}
     ];
